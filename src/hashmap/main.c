e9e1e3ea1e420b3ec92f141e708835f2504df410
michael.goldsmith <michael.goldsmith@datalex.com>
2025-09-22 09:40:28 +0700

    added placeholder template using CMake for the hashmap and memory arena implementations

---
+++ src/hashmap/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Hashmap!\n");
+    return 0;
+}
---
+++ src/memory_arena/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Memory Arena!\n");
+    return 0;
+}
